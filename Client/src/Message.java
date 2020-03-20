584e43b83b7e7a279c7ed34cd4456f7a07db0ff8
Valentina-Andreea <eteganv@gmail.com>
2020-03-20 19:18:59 +0100

    changement message

--- Client/src/Message.java
+++ Client/src/Message.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Message {
+    public String username;
+    public String message;
+    public Date date;
+
+    public Message(String username, String message, Date date){|
+        this.username=username;
+        this.message=message;
+        this.date=date;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public Date getDate(){
+        return date;
+    }
 }
