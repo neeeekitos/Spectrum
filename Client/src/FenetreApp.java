ce7f57789c0d1659e0a005b48d848d35c73e3c73
neeeekitos <nikita.a.terekhov@gmail.com>
2020-03-23 21:58:45 +0100

    FenetreApp

--- Client/src/FenetreApp.java
+++ Client/src/FenetreApp.java
@@ -1,26 +1,51 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FenetreApp extends JFrame implements ActionListener {
 
+    private JFrame frame;
     private Client user;
-    JFrame frame;
-    JTextArea lConsole;
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickname = new JTextField("username");
+    private final JTextField fieldInput = new JTextField("fieldInput");
 
-    public FenetreApp(Client client) {
-        this.user = client;
+    public FenetreApp(Client user) {
+
         frame = new JFrame();
+        this.user = user;
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        lConsole = new JTextArea("dddddd");
-        frame.add(lConsole);
-        frame.setVisible(true);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        log.setEditable(false);
+        log.setLineWrap(true);
+        fieldInput.addActionListener(this);
+        add(log, BorderLayout.CENTER);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickname, BorderLayout.NORTH);
+        setVisible(true);
     }
 
+    public synchronized void printMsg(String msg){ //тк работаем из потока окошка и соединения
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(msg + "\n");
+                log.setCaretPosition(log.getDocument().getLength());
+            }
+        });
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        String msg = fieldInput.getText();
+        if (msg.equals("")) return;
+        fieldInput.setText(null);
+        user.getConnection().sendString(fieldNickname.getText() + " : " + msg);
+        this.validate();
     }
 }
